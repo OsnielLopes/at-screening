15f9a3e44a817112647b317db5598c4a0ea9ccac
Osniel Teixeira <niellt@hotmail.com>
2020-10-23 20:07:54 -0300

    Correcting User import

--- django_project/poller_project/poller_app/forms.py
+++ django_project/poller_project/poller_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.contrib.auth.models import User
 
-from .models import Question, Choice, User, Answer
+from .models import Question, Choice, Answer
 
 
 class AnswerForm(forms.ModelForm):
