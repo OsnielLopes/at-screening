8e7b19e44632dfd8a7037b479055d1c0a3c35d9c
Osniel Teixeira <niellt@hotmail.com>
2020-10-23 19:41:28 -0300

    Import correction

--- django_project/poller_project/urls.py
+++ django_project/poller_project/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
-from django_project.poller_app.admin import site
+from .poller_app.admin import site
 
 admin.site = site
 admin.autodiscover()
